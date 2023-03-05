a4a14c55b903fdfd5e4a329256663cf0595c644e
Keith Gichovi <kgichovi04@gmail.com>
2023-03-05 01:05:20 +0000

    safe commit

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const input = document.getElementById('input');
 
 const inputBtn = document.getElementById('input-btn');
 
-inputBtn.addEventListener("click", function(){
+const ulEl = document.getElementById('uL-el');
+
+console.log(ulEl);
+
+inputBtn.addEventListener("click", function() {
 
     myLeads.push(input.value);
     console.log(myLeads);
 
 });
 
+for(let i = 0; i < myLeads.length; i++){
+
+    ulEl.textContent += myLeads[i];
+
+}
