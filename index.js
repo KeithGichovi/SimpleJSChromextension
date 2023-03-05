aed653d083726dc09d18682a6fb4bc7c6c51939b
Keith Gichovi <kgichovi04@gmail.com>
2023-03-05 22:57:50 +0000

    safe commit

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 let myLeads = [];
 
 const input = document.getElementById('input');
-
 const inputBtn = document.getElementById('input-btn');
-
 const ulEl = document.getElementById('uL-el');
 
-console.log(ulEl);
-
 inputBtn.addEventListener("click", function() {
-
     myLeads.push(input.value);
-    console.log(myLeads);
-
+    renderLeads()
 });
 
-for(let i = 0; i < myLeads.length; i++){
 
-    ulEl.textContent += myLeads[i];
 
+function renderLeads() {
+    let listItems = '';
+
+    for(let i = 0; i < myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
 }
