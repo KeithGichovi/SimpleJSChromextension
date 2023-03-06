bc029f1987e5dd3af9ca6951864516987737db21
Keith <kgichovi04@gmail.com>
2023-03-06 00:09:20 +0000

    final version, 1 error!!

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 let myLeads = [];
-
-const input = document.getElementById('input');
-const inputBtn = document.getElementById('input-btn');
-const ulEl = document.getElementById('uL-el');
+const inputEl = document.getElementById("input");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("uL-el");
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(input.value);
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 });
 
-
-
 function renderLeads() {
-    let listItems = '';
-
-    for(let i = 0; i < myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>";
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
+        listItems +=
+            listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+            </li>
+    `;
+        console.log(listItems);
     }
     ulEl.innerHTML = listItems;
 }
